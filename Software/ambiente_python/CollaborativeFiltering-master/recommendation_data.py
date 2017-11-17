729d7a40e86c082fe90e72c4795d14cf1897a882
levi.moraes <levi.moraesds@gmail.com>
2017-11-17 02:51:13 -0200

    Atualização dos Resultados

---
+++ Software/ambiente_python/CollaborativeFiltering-master/recommendation_data.py
@@ -0,0 +1,26 @@
+dataset={
+			'Gestor 1': {'ISSUES': 2.5, 
+							'DUPLICACAO': 3.5,
+							'PERFORMANCE': 3.0},
+
+			'Gestor 2': {'ISSUES': 3.0, 
+							'WMC': 3.5,
+							'VULNERABILIDADE': 1.5,
+							 'COMENTARIO': 5.0, 
+							 'PERFORMANCE': 3.0,
+							'INFO': 3.5},
+
+			'Gestor 3': {'CODE_SMELLS': 3.5,
+							'VULNERABILIDADE': 3.5,
+							'PERFORMANCE': 2.5,
+							 'ISSUES':3.0,
+							 'LOC': 4},
+
+			'Gestor 4': {'COMENTARIO': 3.5, 
+							'ISSUES': 3.0,
+							'INFO': 4.5},
+			'Usuario':{'CODE_SMELLS': 3.5,
+							'VULNERABILIDADE': 3.5,
+							'PERFORMANCE': 2.5,
+							 'ISSUES':3.0} 
+			}
---
+++ Software/ambiente_python/CollaborativeFiltering-master/recommendation_data_2.py
@@ -0,0 +1,25 @@
+dataset={
+			'Gestor 1': {'ISSUES': 2.5, 
+							'DUPLICACAO': 3.5,
+							'PERFORMANCE': 3.0},
+
+			'Gestor 2': {'ISSUES': 3.0, 
+							'WMC': 3.5,
+							'VULNERABILIDADE': 1.5,
+							 'COMENTARIO': 5.0, 
+							 'BLOCKER': 3.0,
+							'INFO': 3.5},
+
+			'Gestor 3': {'CODE_SMELLS': 2.5, 
+								'VULNERABILIDADE': 3.0,
+								'DUPLICACAO': 3.5,
+								 'BLOCKER': 4.0},
+
+			'Gestor 4': {'COMENTARIO': 3.5, 
+							'ISSUES': 3.0,
+							'INFO': 4.5},
+
+			'Usuario':{'CODE_SMELLS': 3.5,
+							'VULNERABILIDADE': 3.5,
+							'PERFORMANCE': 3.0} 
+			}
